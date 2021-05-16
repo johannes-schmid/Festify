2b795c8909257fba34d5c794e146bf321fd9c69c
Johannes Schmid <schmid.johannes90@gmail.com>
2021-05-16 16:31:32 +0200

    started

--- src/index.py
+++ src/index.py
@@ -2,30 +2,25 @@ from flask import Flask, render_template
 from src.services.SubscriptionManager import SubscriptionManager
 app = Flask(__name__)
 
-changed_events = [
-    {
-        'Name': 'Dekmantel',
-        'Date': '25-04-2021',
-        'Website': 'www.dekmantel.com'
-    },
-    {
-        'Name': 'Event2',
-        'Date': '25-04-2021',
-        'Website': 'www.event2.com'
-    }
-]
-
-test = SubscriptionManager()
-test.subscribe('Hubi','Hansi','hansi@hansi.de')
-
-
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/mail')
-def mail():
-    return render_template("mail.html", events=changed_events)
+@app.route('/subscribe')
+def index():
+    return render_template("subscribe.html")
+
+@app.route('/success')
+def index():
+    return render_template("success.html")
+
+@app.route('/failed')
+def index():
+    return render_template("failed.html")
+
+# @app.route('/mail')
+# def mail():
+#     return render_template("mail.html", events=changed_events)
 
 
 if __name__ == '__main__':
